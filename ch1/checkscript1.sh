455fdf67585349116e384020e175e601d1016330
jmbohan <jmbohan@purdue.edu>
2022-10-02 17:45:22 -0400

    fixed script

--- ch1/checkscript1.sh
+++ ch1/checkscript1.sh
@@ -2,14 +2,14 @@
 
 FILE="/etc/shadow"
 
-if[-e"$FILE"] 
-  then 
+if [ -e "$FILE" ] 
+then
   echo "Shadow passwords are enabled"
 fi
 
-if[-w"$FILE"]
-  then
+if [ -w "$FILE" ]
+then
   echo "You have Permissions to edit ${FILE}."
-  else
+else
   echo "You do NOT have Permissions to edit ${FILE}."
 fi
